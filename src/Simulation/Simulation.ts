afe184d3d1b91323a63406473b1973297c6d8f9c
brianboyko <brian.boyko@gmail.com>
2020-05-21 15:09:20 +0100

    wrote first simulation code (untested)

--- src/Simulation/Simulation.ts
+++ src/Simulation/Simulation.ts
@@ -1,14 +1,13 @@
 import Game from "../Game";
 import Card from "../Deck/Card";
 import { freshDeck } from "../Deck";
+import nChooseK from "../utils/nChooseK";
 
 export class SingleSimulation {
   public hands: Card[][] = [];
   public board: Card[] = [];
-  private readonly deck: Card[] = freshDeck(); // all cards will be in order, no shuffle.
-  private remainingDeck: Card[] = [];
   private deadCards: number[] = [];
-
+  private simulationResults: number[][] = [];
   public pluckFromDeck = (...indexes: number[]) => {
     for (let index of indexes) {
       if (this.deadCards.includes(index)) {
@@ -27,10 +26,38 @@ export class SingleSimulation {
     this.pluckFromDeck(card.index);
     this.board.push(card);
   };
-  public setRemainingDeck = () => {
-    this.remainingDeck = this.deck.filter(
-      (card: Card) => !this.deadCards.includes(card.index)
-    );
-    return this;
-  };
+  public simulateBoard = (): Promise<number[][]> =>
+    new Promise((resolve) => {
+      const remainingStreets = 5 - this.board.length;
+      if (remainingStreets === 0) {
+        const onlyGame = new Game(this.hands.length);
+        onlyGame.hands = this.hands;
+        onlyGame.board = this.board;
+        this.simulationResults = onlyGame.showdown().equity;
+        return resolve(this.simulationResults);
+      }
+
+      const remainingDeck = freshDeck().filter(
+        (card: Card) => !this.deadCards.includes(card.index)
+      );
+      const remainingCards = remainingDeck.length;
+      const allPossibleCombinations: number[][] = nChooseK(
+        remainingCards,
+        remainingStreets
+      );
+      console.log(
+        `simulating ${allPossibleCombinations.length} possible combinations`
+      );
+      for (let combo of allPossibleCombinations) {
+        const gameSim = new Game(this.hands.length);
+        gameSim.hands = this.hands;
+        gameSim.board = this.board.concat(
+          combo.map((index: number) => remainingDeck[index])
+        );
+        this.simulationResults = this.simulationResults.concat(
+          gameSim.showdown().equity
+        );
+      }
+      return resolve(this.simulationResults);
+    });
 }
