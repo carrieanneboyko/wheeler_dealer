86ec7ab29fb1f8b57d83be96e3659e44f88217b2
brianboyko <brian.boyko@gmail.com>
2020-05-23 02:43:23 +0100

    Moved to case sensitive file system, worked on first part of bitwise evaluator

--- src/deck/index.ts
+++
@@ -1,6 +0,0 @@
-import range from "lodash/range";
-import Card from "./Card";
-
-export const DECK = range(52).map((val: number) => new Card(val));
-export const freshDeck = () => DECK.slice();
-export default DECK;
